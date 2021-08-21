fc6af21b84450344017e81f1c98ef28d2ef0f144
Zak <zakmikellides@gmail.com>
2021-08-21 20:47:40 +0100

    Update UI for part info

--- bl3_save_edit_ui/src/views/manage_save/inventory/extra_part_info.rs
+++ bl3_save_edit_ui/src/views/manage_save/inventory/extra_part_info.rs
@@ -1,9 +1,9 @@
-use iced::{container, Color, Column, Container, Length, Row, Text};
+use iced::{container, Color, Column, Text};
 
 use bl3_save_edit_core::resources::ResourcePartInfo;
 
 use crate::bl3_ui::InteractionMessage;
-use crate::resources::fonts::JETBRAINS_MONO;
+use crate::resources::fonts::JETBRAINS_MONO_LIGHT_ITALIC;
 use crate::widgets::row_margin::RowMargin;
 
 pub fn add_extra_part_info<'a>(
@@ -14,73 +14,33 @@ pub fn add_extra_part_info<'a>(
 
     if let Some(effects) = &part_info.effects {
         part_contents_col = part_contents_col.push(RowMargin::create(
-            Container::new(Text::new(effects).font(JETBRAINS_MONO).size(15))
-                .padding(5)
-                .style(PartInfoEffectStyle),
+            Text::new(effects)
+                .font(JETBRAINS_MONO_LIGHT_ITALIC)
+                .color(Color::from_rgb8(180, 180, 180))
+                .size(15),
             2,
         ));
     }
 
-    let mut has_positive_negatives = false;
+    let mut positives_negatives = String::new();
 
-    let mut should_use_separate_rows = false;
+    if let Some(positives) = &part_info.positives {
+        positives_negatives.push_str(positives);
+    }
 
-    let positives_element = if let Some(positives) = &part_info.positives {
-        has_positive_negatives = true;
+    if let Some(negatives) = &part_info.negatives {
+        positives_negatives.push_str(", ");
+        positives_negatives.push_str(negatives);
+    }
 
-        if positives.chars().count() > 50 {
-            should_use_separate_rows = true;
-        }
-
-        Some(RowMargin::create(
-            Container::new(Text::new(positives).font(JETBRAINS_MONO).size(15))
-                .padding(5)
-                .style(PartInfoPositiveStyle),
+    if !positives_negatives.is_empty() {
+        part_contents_col = part_contents_col.push(RowMargin::create(
+            Text::new(positives_negatives)
+                .font(JETBRAINS_MONO_LIGHT_ITALIC)
+                .color(Color::from_rgb8(180, 180, 180))
+                .size(15),
             2,
-        ))
-    } else {
-        None
-    };
-
-    let negatives_element = if let Some(negatives) = &part_info.negatives {
-        has_positive_negatives = true;
-
-        if negatives.chars().count() > 50 {
-            should_use_separate_rows = true;
-        }
-
-        Some(RowMargin::create(
-            Container::new(Text::new(negatives).font(JETBRAINS_MONO).size(15))
-                .padding(5)
-                .style(PartInfoNegativeStyle),
-            2,
-        ))
-    } else {
-        None
-    };
-
-    if has_positive_negatives {
-        if should_use_separate_rows {
-            if let Some(p) = positives_element {
-                part_contents_col = part_contents_col.push(p);
-            }
-
-            if let Some(n) = negatives_element {
-                part_contents_col = part_contents_col.push(n);
-            }
-        } else {
-            let mut positives_negatives_row = Row::new().width(Length::Fill).spacing(2);
-
-            if let Some(p) = positives_element {
-                positives_negatives_row = positives_negatives_row.push(p);
-            }
-
-            if let Some(n) = negatives_element {
-                positives_negatives_row = positives_negatives_row.push(n);
-            }
-
-            part_contents_col = part_contents_col.push(positives_negatives_row);
-        }
+        ));
     }
 
     part_contents_col
