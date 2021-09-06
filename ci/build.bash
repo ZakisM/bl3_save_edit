de2c7ad3ae2a38380f5df2073eb35dfe9f16ef50
Zak <zakmikellides@gmail.com>
2021-09-06 16:02:40 +0100

    Fix Linux CI #10

--- ci/build.bash
+++ ci/build.bash
@@ -6,7 +6,7 @@ source ci/common.bash
 
 # $1 {path} = Path to cross/cargo executable
 CROSS=$1
-# $1 {string} = <Target Triple> e.g. x86_64-pc-windows-msvc
+# $2 {string} = <Target Triple> e.g. x86_64-pc-windows-msvc
 TARGET_TRIPLE=$2
 # $3 {boolean} = Are we building for deployment?
 RELEASE_BUILD=$3
@@ -18,10 +18,6 @@ if [ -z "$RELEASE_BUILD" ]; then
     $CROSS build --target $TARGET_TRIPLE
     $CROSS build --target $TARGET_TRIPLE --all-features
 else
-    if [[ $TARGET_TRIPLE =~ "linux" ]]; then
-      sudo apt install libfreetype6-dev
-    fi
-
     $CROSS build --target $TARGET_TRIPLE --all-features --release
 fi
 
