2c1ea4c57ecadab44f95557ce8643aa793fa7920
Zak <zakmikellides@gmail.com>
2021-09-09 17:37:38 +0100

    Update icon

--- bl3_save_edit_ui/src/resources/fonts.rs
+++ bl3_save_edit_ui/src/resources/fonts.rs
@@ -10,9 +10,9 @@ pub const JETBRAINS_MONO_BOLD: Font = Font::External {
     bytes: include_bytes!("../../resources/font/JetBrainsMono-Bold.ttf"),
 };
 
-pub const JETBRAINS_MONO_NL_EXTRA_BOLD: Font = Font::External {
-    name: "Jetbrains Mono NL Extra Bold",
-    bytes: include_bytes!("../../resources/font/JetBrainsMonoNL-ExtraBold.ttf"),
+pub const JETBRAINS_MONO_NL_EXTRA_BOLD_ITALIC: Font = Font::External {
+    name: "Jetbrains Mono NL Extra Bold Italic",
+    bytes: include_bytes!("../../resources/font/JetBrainsMonoNL-ExtraBoldItalic.ttf"),
 };
 
 pub const JETBRAINS_MONO_LIGHT_ITALIC: Font = Font::External {
